1bb935c3c56bb0be88c20bd9376ce7f472eb4978
azizktata <65952489+azizktata@users.noreply.github.com>
2023-11-12 15:35:05 +0100

    first commit

---
+++ src/main/java/com/example/project/PDS/PdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.PDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/PDS/PdsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.PDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

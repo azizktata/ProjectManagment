22fe4ff178892e36a5362f0405f5e6ec5efd80ec
azizktata <65952489+azizktata@users.noreply.github.com>
2023-11-26 09:01:39 +0100

    updates

--- src/main/java/com/example/project/PDS/controllers/SupervisorController.java
+++ src/main/java/com/example/project/PDS/controllers/SupervisorController.java
@@ -36,7 +36,7 @@ public class SupervisorController {
 
 
     @Operation(summary = "Delete this Supervisor By Id")
-    @DeleteMapping(value ="/{Id}")
+    @DeleteMapping(value ="/ID/{Id}")
     public String removeSupervisor(@PathVariable String Id){return supervisorService.deleteSupervisor(Id);}
 
     @Operation(summary = "Delete this Supervisor By Name")
